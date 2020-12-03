647ff5409502091defd5ae95deda850b322ed091
mdzoubir <57962405+mdzoubir@users.noreply.github.com>
2020-12-03 20:34:30 +0100

    Add files via upload

--- java-basic/test/src/test/produit.java
+++ java-basic/test/src/test/produit.java
@@ -6,21 +6,35 @@ public class produit {
 	static Scanner reader = new Scanner(System.in);
 	public static void main(String[] args) {
 		
-		double 	HT = 0, TVA = 0.2 , TTC = 0.0, r =0;
-		
 		
 		System.out.println("le prix de produit (dh):");
-		HT = reader.nextDouble();
-		System.out.println("le pourcentage de remise :");
-		r = reader.nextDouble();
-		if (r == 0) {
-			TTC = HT + (HT * TVA);
-			System.out.println("TTC = " + TTC + "dh");
+		double HT = reader.nextDouble();
+		double tva = 0.2;
+		if(HT >= 200){
+			System.out.println("demande de remise oui ou yes");
+			String acRremise = reader.next();
+			
+			if(acRremise.equals("oui") || acRremise.equals("yes")){
+				System.out.println("pourcentage de remise : ");
+				double remise = reader.nextDouble();
+				
+				if(remise >= 1){
+					double porRemise = remise / 100;
+					double ttc = HT + (HT * tva) - (porRemise*HT);
+					System.out.println("TTC =" +ttc );
+				}else{
+					double ttc = HT + (HT * tva) - (remise*HT);
+					System.out.println("TTC =" +ttc );
+				}
+			}else{
+				double ttc = HT + (HT * tva);
+				System.out.println("TTC =" +ttc );
+			}
+		}else{
+			double ttc = HT + (HT * tva);
+			System.out.println("TTC =" +ttc );
 		}
-		if(r > 0.0){
-			r = HT * r;
-			TTC = HT + (HT * TVA) - r;
-			System.out.println("TTC = " + TTC + "dh");
-		}
+		
+		
 	}
 }
